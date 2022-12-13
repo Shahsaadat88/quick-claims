9758062076523f33c71197c1424e2958143e2dae
Shahsaadat88 <ssa3z@allstate.com>
2022-12-13 16:06:19 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import List from "./Components/List"
-import './App.css';
-import { useState } from "react";
-import TextField from "@mui/material/TextField";
 
-function App() {
-  const [inputPolicy, setInputPolicy] = useState("");
-  let inputPolicyValue = (entryValue) =>{
-    var entryValueLowerCase = entryValue.target.value.toLowerCase();
-    statsetInputPolicy(entryValueLowerCase);
-  }
+import './App.css';
+
+
+
   return (
     <div className="App-header">
-      <h1>Policy Search</h1>
-      <p>Please type in your 4 digit policy starting with 123</p>
-        <div className="Policy-Search">
-          <TextField policy="outlined-basic" onchange = {inputPolicyValue} Variant="outlined" fullWidth lable="Search" />
-        </div>
-        <List input={inputPolicy}/>
+      <h1>Claim Page</h1>
+     
     </div>
   );
 }
